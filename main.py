263efd5ab64fd6eb51c33c84d4f373b43fa5def6
ryanMathram <168239412+ryanMathram@users.noreply.github.com>
2025-07-28 23:53:11 -0500

    Started implementation of an AI Agent based on the data from bank account, can the user buy something depending the balance in their account.

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import google.generativeai as genai
 import numpy as np
 from dotenv import load_dotenv
 
+
 load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 columns_names = ["Details","Posting Date","Description","Amount", "Type", "Balance","Check or Slip #","Desires"]
 df = pd.read_csv("bank_statements.CSV",names=columns_names)
 model = genai.GenerativeModel("gemini-2.0-flash")
+
 
 try:
     for index, row in df.iterrows():
@@ -16,8 +18,18 @@ try:
             response = model.generate_content(
                 "Is this a want or a need or depends for the following debit charge? Respond with only saying 'need' or 'want'.: " + str(
                     row.get("Description"))).text
-            row["Desires"] = response
+            df.at[index, "Desires"] = response
 except Exception as e:
     print("Quota Reached")
-print(df["Desires"])
+
+money = float(input("How much money do you have: "))
+ques = input("What is your question: ")
+
+print(model.generate_content(f"Based on details on bank statements details {df["Details", 5]} and price {df["Amount"],5}, Can this user purchase this item given the current balance: {money}").text)
+
+
+
+
+
+
 
